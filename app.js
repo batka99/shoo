41dd710b4a0d5e1cf2791977b62ccc3a313098f4
batka <batka.i99@gmail.com>
2020-07-11 02:32:34 +0800

    shoo ergedeg bolson

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var scores = [0, 0];
 
 var roundScore = 0;
 
-var dice = Math.floor(Math.random() * 6) + 1;
+document.getElementById("score-0").textContent = "0";
 
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
-window.document.querySelector(".dice").style.display = "none";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+// document.querySelector(".dice").style.display = "none";
 
-console.log("Шоо: " + dice);
+var inHtmlclass = document.querySelector(".dice");
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  inHtmlclass.style.display = "block";
+  inHtmlclass.src = "dice-" + diceNumber + ".png";
+});
