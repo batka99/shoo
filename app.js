83fe3236563744b9004fb3fc427d52a7e6672998
batka <batka.i99@gmail.com>
2020-07-12 15:07:21 +0800

    togloomiin onoog 100 bolgoloo

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     document.getElementById("score-" + activePlayer).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
       document
         .querySelector(".player-" + activePlayer + "-panel")
