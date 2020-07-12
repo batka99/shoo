446b065e4cbf43daf465c11f10690c18ff89728a
batka <batka.i99@gmail.com>
2020-07-12 12:25:51 +0800

    ylagch todruulah function hiisen

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var activePlayer = 1;
+var activePlayer = 0;
 
 var scores = [0, 0];
 
@@ -18,4 +18,39 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 
   inHtmlclass.style.display = "block";
   inHtmlclass.src = "dice-" + diceNumber + ".png";
+
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    switchToNextPlayer();
+  }
 });
+// hold event
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 20) {
+    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("active");
+  } else {
+    switchToNextPlayer();
+  }
+});
+
+function switchToNextPlayer() {
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  inHtmlclass.style.display = "none";
+}
